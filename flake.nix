35f7fd441ea676bf88d6abec3a3f4d97151d2bb9
Eva Emmerich <eva@evaemmerich.com>
2025-11-05 14:34:51 +0100

    move shell into flake.nix, add .envrc for direnv

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "ndisk development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        rustc
+        cargo
+        gcc
+        rustfmt
+        clippy
+        rustPlatform.bindgenHook # required for libparted, sets LIBCLANG_PATH and friends
+        rustPlatform.cargoSetupHook
+      ];
+      buildInputs = with pkgs; [
+        parted # for libparted
+        libclang # for libparted-sys
+      ];
+
+      # Certain Rust tools won't work without this
+      # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+      # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
+}
